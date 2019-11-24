dbb38e91bacde1d4212b3ea604e22c8261561254
Boris Ulyanov <boris-ulyanov@mail.ru>
2019-11-24 11:43:43 +0300

    1 - map + default allocator

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 
 #include "myalloc.h"
 
+#include <map>
+
+struct hard {
+    int fa;
+    int fi;
+    hard(int fa, int fi) : fa(fa), fi(fi){};
+};
+
+std::ostream& operator<<(std::ostream& os, const hard& h) {
+    os << "hard(" << h.fa << ":" << h.fi << ")";
+    return os;
+}
+
 int main() {
     const int N = 10;
     int factorial[N];
@@ -13,9 +26,18 @@ int main() {
         fibonacci[i] = (i < 2) ? i : fibonacci[i - 1] + fibonacci[i - 2];
     }
 
-    for (int i = 0; i < N; ++i) {
-        std::cout << i << " -> " << factorial[i] << "\t" << fibonacci[i] << std::endl;
-    }
+    // 1 - map + default allocator
+    std::map<int, hard> map_default;
+    for (int i = 0; i < N; ++i)
+        map_default.emplace(std::make_pair(i, hard{factorial[i], fibonacci[i]}));
+
+    // 2 - map + my allocator
+    // dump
+
+    // 3 - my container + default allocator
+
+    // 3 - my container + my allocator
+    // dump
 
     return 0;
 }
