d7dc61c12be74a4408dda3a78c90af646c70ee4c
Boris Ulyanov <boris-ulyanov@mail.ru>
2019-11-24 11:00:56 +0300

    factorial + fibonacci

--- main.cpp
+++ main.cpp
@@ -2,8 +2,20 @@
 #include "myalloc.h"
 
 int main() {
+    const int N = 10;
+    int factorial[N];
+    int fibonacci[N];
 
-    std::cout << "xxx" << std::endl;
+    // ? не придумал как сгенерить в compile time
+    static_assert(N <= 12, "int overflow");
+    for (int i = 0; i < N; ++i) {
+        factorial[i] = (i < 2) ? 1 : factorial[i - 1] * i;
+        fibonacci[i] = (i < 2) ? i : fibonacci[i - 1] + fibonacci[i - 2];
+    }
+
+    for (int i = 0; i < N; ++i) {
+        std::cout << i << " -> " << factorial[i] << "\t" << fibonacci[i] << std::endl;
+    }
 
     return 0;
 }
